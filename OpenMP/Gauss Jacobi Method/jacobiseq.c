4c39c9950eb853a3b7f32578733e77cbefc8cfc0
MatheusVBellini <matheus.bellini02@usp.br>
2024-04-18 19:30:42 -0300

    Untested convergence function

--- OpenMP/Gauss Jacobi Method/jacobiseq.c
+++ OpenMP/Gauss Jacobi Method/jacobiseq.c
@@ -1,7 +1,43 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define CLI_ARG_NUM 3
+
+int N = 0;
+
+/**
+ * Test if matrix converges in the Jacobi-Gauss method
+ *
+ * @param matrix float**
+ */
+char convergence_test(float **matrix) {
+  /* lines criterium
+   *
+   * a_k = ( \sum_{j=1 && j!=k}^{n} |a_{ik}| ) / |a_{kk}|
+   * max(a_k) < 1
+   *
+   */
+
+  float max = -1;
+  float coeficients[N];
+
+  for (int i = 0; i < N; i++) {
+
+    coeficients[i] = 0;
+    for (int j = 0; j < N; j++) {
+      if (i == j)
+        continue;
+      coeficients[i] += fabs(matrix[i][j]);
+    }
+    coeficients[i] /= fabs(matrix[i][i]);
+
+    if (coeficients[i] > max)
+      max = coeficients[i];
+  }
+
+  return max < 1;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -18,6 +54,11 @@ int main(int argc, char *argv[]) {
     printf("seed -> Seed for random number generations\n");
     exit(1);
   }
+
+  // using arguments
+  N = atoi(argv[1]);
+
+  // DEBUG: convergence_test
 
   return 0;
 }
