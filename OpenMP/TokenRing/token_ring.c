4545d24ee1e70ca5149b2cc9c2e6496af89d07b5
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2024-04-20 23:29:02 -0300

    no need to use id valeus

--- OpenMP/TokenRing/token_ring.c
+++ OpenMP/TokenRing/token_ring.c
@@ -11,21 +11,15 @@ int main() {
 
 #pragma omp parallel
   {
-// fill up id vector
-#pragma omp for
-    for (int i = 0; i < NODENUM; i++) {
-      id[i] = i;
-    }
-
 // start ring
 #pragma omp single
     {
 #pragma omp task depend(out : id[0])
-      { node_task(id[0]); }
+      { node_task(0); }
 
       for (int i = 1; i < NODENUM; i++) {
 #pragma omp task depend(in : id[i - 1]) depend(out : id[i])
-        { node_task(id[i]); }
+        { node_task(i); }
       }
     }
   }
