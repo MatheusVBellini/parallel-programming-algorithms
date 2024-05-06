bc292ada2a331e23423fb14727b4027328096193
MatheusVBellini <matheus.bellini02@usp.br>
2024-05-06 09:32:34 -0300

    jacobipar broke

--- OpenMP/GaussJacobiMethod/benchmark.sh
+++ OpenMP/GaussJacobiMethod/benchmark.sh
@@ -15,8 +15,21 @@ for N in "${test_list[@]}"; do
   for ((i = 1; i <= exec_num; i++)); do
     random_seed=$(( (RANDOM % 1000) + 1 ))                      # generate random seed for the code
     random_choice=$(( RANDOM % $N ))                            # generate random choice for input
-    t=$({ time ./jacobiseq "$N" 1 "$random_seed" <<< "$random_choice"; } 2>&1 3>&1 >/dev/null | grep -oP '(?<= )[0-9]+\,[0-9]+(?= total)')  # returns only turnaround time
+    t=$({ time ./jacobiseq "$N" 1 "$random_seed" <<< "$random_choice"; } 2>&1 3>&1 >/dev/null | grep -oP '(?<= )[0-9]+\,[0-9]+(?= total)')      # returns only turnaround time
     echo "$t" >> "./logs/SEQ-N$N.txt"
+  done
+done
+
+# parallel execution
+for T in "${Ts[@]}"; do
+  for N in "${test_list[@]}"; do
+    echo "[\"PAR-T$T-N$N\"]" > "./logs/PAR-T$T-N$N.txt"           # create log file to save time values
+    for ((i = 1; i <= exec_num; i++)); do
+      random_seed=$(( (RANDOM % 1000) + 1 ))                      # generate random seed for the code
+      random_choice=$(( RANDOM % $N ))                            # generate random choice for input
+      t=$({ time ./jacobipar "$N" "$T" "$random_seed" <<< "$random_choice"; } 2>&1 3>&1 >/dev/null | grep -oP '(?<= )[0-9]+\,[0-9]+(?= total)')  # returns only turnaround time
+      echo "$t" >> "./logs/PAR-T$T-N$N.txt"
+    done
   done
 done
 
