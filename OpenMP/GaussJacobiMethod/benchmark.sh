3d86029c46bf683eeb6dbdb3cc40c3017a51123f
MatheusVBellini <matheus.bellini02@usp.br>
2024-05-06 04:59:27 -0300

    needed vars

--- OpenMP/GaussJacobiMethod/benchmark.sh
+++ OpenMP/GaussJacobiMethod/benchmark.sh
@@ -3,8 +3,13 @@
 # compile
 make all > /dev/null 2>&1
 
+# execution schedule
+Ns=(100 1000 10000)
+Ts=(2 4 8)
+exec_num=30
+
 # sequential execution
-{ time ./jacobiseq 1000 4 4 <<< 0; } 2>&1 3>&1 >/dev/null | grep -oP '(?<= )[0-9]+\,[0-9]+(?= total)'
+{ time ./jacobiseq 10000 4 4 <<< 0; } 2>&1 3>&1 >/dev/null | grep -oP '(?<= )[0-9]+\,[0-9]+(?= total)'  # returns only turnaround time
 
 # clean folder
 make clean > /dev/null
