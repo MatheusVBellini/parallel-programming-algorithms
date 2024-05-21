cfafd84c4a70056e38b63b9019406d7c54bffa97
MatheusVBellini <matheus.bellini02@usp.br>
2024-05-21 19:56:20 -0300

    better token management

--- MPI/TokenRing/token_ring.c
+++ MPI/TokenRing/token_ring.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INC 2
+
 int main(int argc, char *argv[]) {
   int np, myrank;
   int token = 0;
@@ -11,23 +13,26 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
+  int source = (myrank-1) % np;
+  int destination = (myrank+1) % np;
+
   if (myrank == 0) {
     printf("Starting Token-Ring with %d nodes...\n\n", np);
     printf("Node %d received token with value %d\n", myrank, token);
 
-    token++;
-    MPI_Send(&token, 1, MPI_INT, (token % np), (token % np), MPI_COMM_WORLD); // send to next node
+    token += INC;
+    MPI_Send(&token, 1, MPI_INT, destination, destination, MPI_COMM_WORLD); // send to next node
 
-    MPI_Recv(&token, 1, MPI_INT, np-1, 0, MPI_COMM_WORLD, &status); // receive message
+    MPI_Recv(&token, 1, MPI_INT, source, myrank, MPI_COMM_WORLD, &status); // receive message
     printf("\nToken-Ring finished!\n");
 
   } else {
 
-    MPI_Recv(&token, 1, MPI_INT, myrank-1, myrank, MPI_COMM_WORLD, &status); // receive message
+    MPI_Recv(&token, 1, MPI_INT, source, myrank, MPI_COMM_WORLD, &status); // receive message
     printf("Node %d received token with value %d\n", myrank, token);
 
-    token++;
-    MPI_Send(&token, 1, MPI_INT, token % np, token % np, MPI_COMM_WORLD); // send to next node
+    token += INC;
+    MPI_Send(&token, 1, MPI_INT, destination, destination, MPI_COMM_WORLD); // send to next node
 
   }
 
